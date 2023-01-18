342906e46a7f9af0fa8e8eae95065a7ca09be81a
Tomas <tomasluquegonzalez@gmail.com>
2023-01-18 13:29:30 +0100

    update nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
                 <div>
                     <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
                         <Bars4Icon
-                            className="-mx-1 h-5 w-5 text-slate-200 hover:text-slate-100"
+                            className="-mx-1 h-8 w-8 text-slate-200 hover:text-slate-100"
                             aria-hidden="true"
                         />
                     </Menu.Button>
@@ -24,7 +24,7 @@ export default function Navbar() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                 >
-                    <Menu.Items className="absolute right-0 bottom-12 mt-2 w-56 origin-bottom-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute right-0 bottom-16 mt-2 w-56 origin-bottom-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <div className="px-1 py-1 ">
                             <Menu.Item>
                                 {({ active }) => (
@@ -34,9 +34,9 @@ export default function Navbar() {
                                                 active
                                                     ? "bg-sky-500 text-white"
                                                     : "text-slate-900"
-                                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                            } group flex w-full items-center rounded-md px-2 py-2`}
                                         >
-                                            Home
+                                            Accueil
                                         </button>
                                     </Link>
                                 )}
@@ -51,7 +51,7 @@ export default function Navbar() {
                                                 active
                                                     ? "bg-sky-500 text-white"
                                                     : "text-slate-900"
-                                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                            } group flex w-full items-center rounded-md px-2 py-2`}
                                         >
                                             Sorts
                                         </button>
@@ -68,7 +68,7 @@ export default function Navbar() {
                                                 active
                                                     ? "bg-sky-500 text-white"
                                                     : "text-slate-900"
-                                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                            } group flex w-full items-center rounded-md px-2 py-2`}
                                         >
                                             Dons
                                         </button>
